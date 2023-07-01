43238fd38137b41288012ab312d7838592e5281e
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-07-01 13:51:40 +0300

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    def show
+          user = User.includes(:items).find(params[:id])
+          render json: {
+            id: user.id,
+            username: user.username,
+            city: user.city,
+            items: user.items.map do |item|
+                {
+                    id: user.id,
+                    name: item.name,
+                    description: item.description,
+                    price: item.price
+                }
+            end
+          }
+    end
 end
